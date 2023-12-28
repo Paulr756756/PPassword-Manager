c5620fa7fb202d3f2a12a4e27ce61dff5a943233
Johann <paulr756756@gmail.com>
2023-12-28 18:59:43 +0530

    Added a vue client

---
+++ ppm_client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Card from './components/Card.vue';
+import Home from './components/Home.vue';
+import Hero from './components/Hero.vue';
+</script>
+
+<template>
+  <Hero/>
+</template>
+
+<style scoped>
+</style>
